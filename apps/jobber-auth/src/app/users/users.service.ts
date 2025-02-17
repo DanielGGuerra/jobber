b51455bac049eceaf4434fa28be22b1fa8cf400b
danielgguerra <danielgguerra2@gmail.com>
2025-02-17 15:26:39 -0300

    auth/login

--- apps/jobber-auth/src/app/users/users.service.ts
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -5,18 +5,22 @@ import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-    async createUser(data: Prisma.UserCreateInput) {
-        return await this.prismaService.user.create({ 
-            data: {
-                ...data,
-                password: await hash(data.password, 10)
-            }
-         });
-    }
+  async createUser(data: Prisma.UserCreateInput) {
+    return await this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10),
+      },
+    });
+  }
 
-    async getUsers() {
-        return await this.prismaService.user.findMany();
-    }
+  async getUsers() {
+    return await this.prismaService.user.findMany();
+  }
+
+  async getUser(args: Prisma.UserWhereUniqueInput) {
+    return await this.prismaService.user.findUniqueOrThrow({ where: args });
+  }
 }
--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -6,10 +6,12 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,9 +19,13 @@ async function bootstrap() {
       transform: true,
     })
   );
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = app.get(ConfigService).getOrThrow('PORT');
+
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );

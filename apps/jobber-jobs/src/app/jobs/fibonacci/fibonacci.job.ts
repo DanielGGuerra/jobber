0da0cf2053d3b0019475783c7a5a04bebfebbba7
danielgguerra <danielgguerra2@gmail.com>
2025-02-19 11:52:25 -0300

    job validation

--- apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
+++ apps/jobber-jobs/src/app/jobs/fibonacci/fibonacci.job.ts
@@ -1,13 +1,14 @@
 import { PulsarClient } from '@jobber/pulsar';
 import { Job } from '../../decorators/job.decorator';
 import { AbstractJob } from '../abstract.job';
-import { FibonacciData } from './fibonacci-data.interface';
+import { FibonacciData } from './fibonacci-data.message';
 
 @Job({
   name: 'Fibonacci',
   description: 'Generate a Fibonacci sequence and to it in the database',
 })
 export class FibonacciJob extends AbstractJob<FibonacciData> {
+  protected messageClass = FibonacciData;
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient);
   }

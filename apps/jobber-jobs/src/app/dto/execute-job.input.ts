d078fecb49b8a88c77765fc0c8a926357cf7e92b
danielgguerra <danielgguerra2@gmail.com>
2025-02-19 11:02:01 -0300

    graphql-type-json

--- apps/jobber-jobs/src/app/dto/execute-job.input.ts
+++ apps/jobber-jobs/src/app/dto/execute-job.input.ts
@@ -1,9 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsNotEmptyObject } from 'class-validator';
+import JSON from 'graphql-type-json';
 
 @InputType()
 export class ExecuteJobInput {
   @Field()
   @IsNotEmpty()
   name: string;
+
+  @Field(() => JSON)
+  @IsNotEmptyObject()
+  data: object;
 }
